d136f1b1d3be4303569f872ae37c0fa3542c3d8a
PiotrPrus <pprus94@gmail.com>
2022-07-29 13:07:06 +0200

    Problem with id

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const advice = document.querySelector('.advice')
 const btn = document.querySelector('.card-circle')
+const id = document.querySelector('.advice-id')
 
 const URL = 'https://api.adviceslip.com/advice'
 
@@ -7,7 +8,7 @@ btn.addEventListener('click', () => {
 	console.log('ok')
 	fetch(URL)
 		.then(res => res.json())
-		.then(data => advice.innerHTML = data.slip.advice)
+		.then(data => advice.innerHTML = data.slip.advice, {(data => id.innerHTML = data.slip.id)})
 		.catch(error => console.log(error))
 })
 
